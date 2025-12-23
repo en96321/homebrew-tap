7ed2dd58254e11959988b257c21228ffc0062f2f
Pedro Yang <en96321@gmail.com>
2025-12-23 23:00:28 +0800

    Update vip-f2e-tool.rb

--- Casks/vip-f2e-tool.rb
+++ Casks/vip-f2e-tool.rb
@@ -3,7 +3,7 @@
 
 cask "vip-f2e-tool" do
   version "1.0.1"
-  sha256 "828a96b4fe8005d167fdd6b9339b40cdefeb6ed9c7b81ed3932cb72dfabc8e8e"
+  sha256 "b959eb83cfe326f9910a83f0dd44c97cb5d8e6bf5a0574d749f368bcb497158e"
 
   url "https://github.com/en96321/homebrew-tap/releases/download/vip-f2e-tool-#{version}/VIP-F2E-Tool-#{version}.dmg"
   name "VIP F2E Tool"
